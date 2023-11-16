8d65d2b19cab565459e24bcdfde348f8f2627b67
Sudhirkr1669 <101310348+Sudhirkr1669@users.noreply.github.com>
2023-11-16 07:12:18 +0530

    update

--- CyclicSort.java
+++ CyclicSort.java
@@ -12,14 +12,13 @@ public class CyclicSort {
     public static void cyclicSort(int[] nums) {
         int i = 0;
         while (i < nums.length) {
-            // Check if the current element is in the correct position
             if (nums[i] != i + 1) {
-                // Swap the element to its correct position
+                
                 int temp = nums[i];
                 nums[i] = nums[temp - 1];
                 nums[temp - 1] = temp;
             } else {
-                // Move to the next position
+            
                 i++;
             }
         }
